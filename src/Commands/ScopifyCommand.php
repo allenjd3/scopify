65944a1f89841b7ad9f9a40634c58595bb4489be
allenjd3 <allenjd3@users.noreply.github.com>
2023-10-30 00:47:34 +0000

    Fix styling

--- src/Commands/ScopifyCommand.php
+++ src/Commands/ScopifyCommand.php
@@ -13,16 +13,18 @@ class ScopifyCommand extends Command implements PromptsForMissingInput
 
     public function handle(): int
     {
-        $stubPath = __DIR__ . '/../stubs/NameFilter.php.stub';
-        $destinationPath = app_path('Filters/' . $this->generateFilterName() . '.php');
+        $stubPath = __DIR__.'/../stubs/NameFilter.php.stub';
+        $destinationPath = app_path('Filters/'.$this->generateFilterName().'.php');
 
         if (! file_exists($destinationPath)) {
             $this->makeDirectory($destinationPath);
             file_put_contents($destinationPath, $this->compileStub($stubPath));
             $this->info('Filter created successfully.');
+
             return 1;
         } else {
             $this->error('Filter already exists.');
+
             return 0;
         }
     }
@@ -32,6 +34,7 @@ class ScopifyCommand extends Command implements PromptsForMissingInput
         $stub = file_get_contents($stubPath);
         $stub = str_replace('{{ name }}', $this->generateFilterName(), $stub);
         $stub = str_replace('{{ namespace }}', config('scopify.default-namespace'), $stub);
+
         return $stub;
     }
 
@@ -42,10 +45,10 @@ class ScopifyCommand extends Command implements PromptsForMissingInput
         }
     }
 
-    protected  function generateFilterName(): string
+    protected function generateFilterName(): string
     {
         return str($this->argument('name'))->endsWith('Filter')
             ? str($this->argument('name'))->title()
-            : str($this->argument('name'))->title() . 'Filter';
+            : str($this->argument('name'))->title().'Filter';
     }
 }
