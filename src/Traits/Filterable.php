8fbfe4c99a80f252d4c4f1e4780485609ecc4671
James Allen <jacques2186@yahoo.com>
2023-10-29 21:48:50 -0400

    Add filterable trait
    Also updates the stub with more information

---
+++ src/Traits/Filterable.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Allenjd3\Scopify\Traits;
+
+use Illuminate\Support\Collection;
+
+trait Filterable
+{
+    protected abstract function scopifyFilters(): Collection;
+
+    public function hasNamedScope($scope)
+    {
+        return method_exists($this, 'scope'.ucfirst($scope))
+            || $this->hasScopifyFilter($scope);
+    }
+
+    public function callNamedScope($scope, array $parameters = [])
+    {
+        if ($this->hasScopifyFilter($scope)) {
+            return $this->getScopifyFilter($scope)->call(...$parameters);
+        }
+
+        return parent::callNamedScope($scope, $parameters);
+    }
+
+    private function hasScopifyFilter($scope)
+    {
+        return $this->scopifyFilters()->has($scope);
+    }
+
+    private function getScopifyFilter($scope)
+    {
+        return new ($this->scopifyFilters()->get($scope));
+    }
+}
